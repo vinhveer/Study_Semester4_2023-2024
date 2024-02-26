3587be42dddd1e5665be85943b9d77785d99209e
vinhveer <63202399+vinhveer@users.noreply.github.com>
2024-02-26 13:20:02 +0700

    upload

--- ComputerGraphics/hello.cpp
+++ ComputerGraphics/hello.cpp
@@ -1,7 +1,8 @@
 #include <graphics.h>
 #include <cmath>
 
-void drawLineDDA(int X1, int Y1, int X2, int Y2) {
+void drawLineDDA(int X1, int Y1, int X2, int Y2)
+{
     int dx = X2 - X1;
     int dy = Y2 - Y1;
 
@@ -13,16 +14,18 @@ void drawLineDDA(int X1, int Y1, int X2, int Y2) {
     float X = X1;
     float Y = Y1;
 
-    for (int i = 0; i <= steps; i++) {
+    for (int i = 0; i <= steps; i++)
+    {
         putpixel(round(X), round(Y), WHITE);
         X += Xincrement;
         Y += Yincrement;
     }
 }
 
-int main() {
+int main()
+{
     int gd = DETECT, gm;
-	initwindow(500, 500);
+    initwindow(500, 500);
 
     int X1 = 100, Y1 = 100, X2 = 300, Y2 = 200;
     drawLineDDA(X1, Y1, X2, Y2);
