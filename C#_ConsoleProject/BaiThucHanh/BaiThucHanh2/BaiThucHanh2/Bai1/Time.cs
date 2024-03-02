08753fffe0bb04ec24ec1a35d1b54b8ab01c9aa5
vinhveer <63202399+vinhveer@users.noreply.github.com>
2024-03-02 16:55:01 +0700

    Upload

---
+++ C#_ConsoleProject/BaiThucHanh/BaiThucHanh2/BaiThucHanh2/Bai1/Time.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Bai1
+{
+    internal class Time
+    {
+        private int hour;
+        private int minute;
+        private int second;
+
+        public Time()
+        {
+            hour = 0;
+            minute = 0;
+            second = 0;
+        }
+
+        public Time(int h, int m, int s)
+        {
+            hour = h;
+            minute = m;
+            second = s;
+        }
+
+        public int Minute
+        {
+            get { return minute; }
+            set { minute = value; }
+        }
+
+        public int Second
+        {
+            get { return second; }
+            set { second = value; }
+        }
+
+        public int GetHour()
+        {
+            return hour;
+        }
+
+        public void SetHour(int i)
+        {
+            hour = i;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("{0}:{1}:{2}", hour, minute, second);
+        }
+
+    }
+}
